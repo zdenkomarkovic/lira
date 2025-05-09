147fc0467cc48e28da3efe03404957925da892e3
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-05-09 08:42:19 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://zastitanaradu.rs", // Zameni svojim domenom
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
